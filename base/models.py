ea4d4ab80a27b8ff841bc5c07cf4ef6f8cb8eaca
Swannhs <princeapurbask@gmail.com>
2021-02-28 11:58:36 -0500

    Performed Django Model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=40, null=True, blank=True)
+    # image
+    category = models.CharField(max_length=30, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
