53aae4e1820cf66e84188951c8506604f7039988
Swannhs <princeapurbask@gmail.com>
2021-02-28 08:44:35 -0500

    Playing with some static rest api in Django

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from base import views
 
 urlpatterns = [
-    path('', views.getProducts, name='routes')
+    path('', views.getRoutes, name='routes'),
+    path('products/', views.getProducts, name='products'),
+    path('products/<str:id>', views.getProduct, name='product'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from base.products import products
 
 
@@ -27,3 +28,14 @@ def getRoutes(request):
 @api_view(['GET'])
 def getProducts(request):
     return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request, id):
+    product = None
+
+    for i in products:
+        if i['_id'] == id:
+            product = i
+            break
+    return Response(product)
