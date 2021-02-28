5d5c2f9ef84d7ae705aaf713669668279e3bfc10
Swannhs <princeapurbask@gmail.com>
2021-02-28 12:25:21 -0500

    Complete with full database structure

--- base/models.py
+++ base/models.py
@@ -17,3 +17,52 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=40, null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.rating)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=40, null=True, blank=True)
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    isDelivered = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=40, null=True, blank=True)
+    quality = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=40, null=True, blank=True)
+    city = models.CharField(max_length=40, null=True, blank=True)
+    postalCode = models.CharField(max_length=40, null=True, blank=True)
+    country = models.CharField(max_length=40, null=True, blank=True)
+    shippingPrice = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.address)
