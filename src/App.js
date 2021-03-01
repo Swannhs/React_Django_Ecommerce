7821d51579be34a74778eba9ac510211f5eaaecb
Swannhs <princeapurbask@gmail.com>
2021-03-01 14:53:24 -0500

    Getting data using Redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Products from "./components/layout/Products";
 
+
 function App() {
     return (
         <div className="App">
--- src/components/layout/Products.js
+++ src/components/layout/Products.js
@@ -1,56 +1,44 @@
-import React, {Component} from 'react';
-import DjangoApi from "../RestApi/DjangoApi";
+import React, {useState, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {listProducts} from "../../actions/ProductActions";
 
-class Products extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
-    }
+function Products() {
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [])
 
-    componentDidMount() {
-        DjangoApi.get('/products/')
-            .then(response => {
-                this.setState({
-                    data: response.data
-                })
-                console.log(response.data)
-            })
-    }
 
-    render() {
+    return (
+
+        products.map((item) => {
         return (
-            <>
-                {
-                    this.state.data.map((item) => {
-                        return (
-                            <div className="ui card">
-                                <div className="image">
-                                    <img src={'http://127.0.0.1:8000' + item.image}/>
-                                </div>
-                                <div className="content">
-                                    <a className="header">{item.name}</a>
-                                    <div className="meta">
-                                        <span className="date">Joined in 2013</span>
-                                    </div>
-                                    <div className="description">
-                                        {item.description}
-                                    </div>
-                                </div>
-                                <div className="extra content">
-                                    <a>
-                                        <i className="user icon"/>
-                                        22 Friends
-                                    </a>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </>
-        );
-    }
+            <div className="ui card" key={item.id}>
+                <div className="image">
+                    <img alt='' src={'http://127.0.0.1:8000' + item.image}/>
+                </div>
+                <div className="content">
+                    <p className="header">{item.name}</p>
+                    <div className="meta">
+                        <span className="date">Joined in 2013</span>
+                    </div>
+                    <div className="description">
+                        {item.description}
+                    </div>
+                </div>
+                <div className="extra content">
+                    <p>
+                        <i className="user icon"/>
+                        22 Friends
+                    </p>
+                </div>
+            </div>
+        )
+    })
+    )
+
 }
 
-export default Products;
+export default Products
